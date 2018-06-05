099a4f36c22dc401f7e4cf6fb295bca594a466ce
Michael Garland <mgarland@pmgnet.local>
2018-06-05 14:38:19 -0400

    smoothy

--- public/js/aws.js
+++ public/js/aws.js
@@ -140,3 +140,77 @@ var s3 = new AWS.S3({
       console.log('you made it here ass-wipe');
     });
   }
+
+  function runTableQuery() {
+
+    // Here we get the location of the root page.
+    // We use this instead of explicitly saying the URL is localhost:3001 because the url will change when we deploy.
+    var currentURL = window.location.origin;
+
+    // The AJAX function uses the URL of our API to GET the data associated with it (initially set to localhost)
+    $.ajax({ url: currentURL + "/api/tables", method: "GET" })
+      .then(function(tableData) {
+
+      // Here we are logging the URL so we have access to it for troubleshooting
+        console.log("------------------------------------");
+        console.log("URL: " + currentURL + "/api/tables");
+        console.log("------------------------------------");
+
+        // Here we then log the NYTData to console, where it will show up as an object.
+        console.log(tableData);
+        console.log("------------------------------------");
+
+        // Loop through and display each of the customers
+        for (var i = 0; i < tableData.length; i++) {
+
+        // Create the HTML Well (Section) and Add the table content for each reserved table
+          var tableSection = $("<div>");
+          tableSection.addClass("well");
+          tableSection.attr("id", "tableWell-" + i + 1);
+          $("#tableSection").append(tableSection);
+
+          var tableNumber = i + 1;
+
+
+          // Then display the remaining fields in the HTML (Section Name, Date, URL)
+          $("#tableWell-" + i + 1).append("<h2><span class='label label-primary'>" + tableNumber + "</span> | " + tableData[i].customerID + "</h2>");
+        }
+      });
+  }
+
+  function viewDankMemes() {
+
+    // Here we get the location of the root page.
+    // We use this instead of explicitly saying the URL is localhost:3001 because the url will change when we deploy.
+    var currentURL = window.location.origin;
+
+    // The AJAX function uses the URL of our API to GET the data associated with it (initially set to localhost)
+    $.ajax({ url: currentURL + "/api/memes", method: "GET" })
+      .then(function(waitlistData) {
+
+      // Here we are logging the URL so we have access to it for troubleshooting
+        console.log("------------------------------------");
+        console.log("URL: " + currentURL + "/api/waitlist");
+        console.log("------------------------------------");
+
+        // Here we then log the NYTData to console, where it will show up as an object.
+        console.log(waitlistData);
+        console.log("------------------------------------");
+
+        // Loop through and display each of the customers
+        for (var i = 0; i < waitlistData.length; i++) {
+
+        // Create the HTML Well (Section) and Add the table content for each reserved table
+          var waitlistSection = $("<div>");
+          waitlistSection.addClass("well");
+          waitlistSection.attr("id", "waitlistWell-" + i + 1);
+          $("#waitlistSection").append(waitlistSection);
+
+          var tableNumber = i + 1;
+
+          // Then display the remaining fields in the HTML (Section Name, Date, URL)
+          $("#waitlistWell-" + i + 1).append("<h2><span class='label label-primary'>" + tableNumber + "</span> | " + waitlistData[i].customerID + "</h2>");
+        }
+      });
+
+  }
